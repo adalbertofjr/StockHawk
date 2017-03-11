525bd958659cb3c2bf9cd269a45f66ff20fbdabb
adalbertofjr <adalberto@sysmobil.com.br>
2017-03-11 14:59:52 -0300

    Recuperando dados da ação em detail

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -8,10 +8,20 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
+import com.udacity.stockhawk.data.PrefUtils;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import timber.log.Timber;
 
 /**
@@ -21,20 +31,42 @@ import timber.log.Timber;
  */
 
 public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-
-
     private static final int STOCK_LOADER = 0;
+
+    @BindView(R.id.symbol)
+    TextView mSymbol;
+
+    @BindView(R.id.price)
+    TextView mPrice;
+
+    @BindView(R.id.change)
+    TextView mChange;
+
+    private DecimalFormat dollarFormat;
+    private DecimalFormat dollarFormatWithPlus;
+    private DecimalFormat percentageFormat;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        ButterKnife.bind(this);
+
+        dollarFormat = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+        dollarFormatWithPlus = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+        dollarFormatWithPlus.setPositivePrefix("+$");
+        percentageFormat = (DecimalFormat) NumberFormat.getPercentInstance(Locale.getDefault());
+        percentageFormat.setMaximumFractionDigits(2);
+        percentageFormat.setMinimumFractionDigits(2);
+        percentageFormat.setPositivePrefix("+");
+
         getSupportLoaderManager().initLoader(STOCK_LOADER, null, this);
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Timber.d("onCreateLoader started...");
         String symbol = getIntent().getExtras().getString(Intent.EXTRA_TEXT);
 
         if (symbol == null) {
@@ -61,7 +93,46 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
         cursor.moveToNext();
 
         String symbol = cursor.getString(Contract.Quote.POSITION_SYMBOL);
-        Timber.i("Stock detail: " + symbol);
+
+        mSymbol.setText(symbol);
+        mSymbol.setContentDescription(getString(R.string.a11y_stock, symbol));
+
+        mPrice.setText(dollarFormat.format(cursor.getFloat(Contract.Quote.POSITION_PRICE)));
+
+        float rawAbsoluteChange = cursor.getFloat(Contract.Quote.POSITION_ABSOLUTE_CHANGE);
+        float percentageChange = cursor.getFloat(Contract.Quote.POSITION_PERCENTAGE_CHANGE);
+
+        if (rawAbsoluteChange > 0) {
+            mChange.setBackgroundResource(R.drawable.percent_change_pill_green);
+        } else {
+            mChange.setBackgroundResource(R.drawable.percent_change_pill_red);
+        }
+
+        String change = dollarFormatWithPlus.format(rawAbsoluteChange);
+        String percentage = percentageFormat.format(percentageChange / 100);
+
+        if (PrefUtils.getDisplayMode(this)
+                .equals(getString(R.string.pref_display_mode_absolute_key))) {
+            mChange.setText(change);
+        } else {
+            mChange.setText(percentage);
+        }
+
+        String history = cursor.getString(Contract.Quote.POSITION_HISTORY);
+        //getHistory(history);
+    }
+
+    private void getHistory(String history) {
+        String[] dateHistorys = history.split("\n");
+        Map<Long, Float> pricesHistory = new HashMap<>();
+
+        for (String priceHistory : dateHistorys) {
+            long date = Long.parseLong(priceHistory.split(",")[0]);
+            float price = Float.parseFloat(priceHistory.split(",")[1]);
+            pricesHistory.put(date, price);
+        }
+
+        Timber.i("Date: " + pricesHistory.toString());
     }
 
     @Override
