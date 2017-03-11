52e72f249df79ea515761be51d48e562fce77141
adalbertofjr <adalberto@sysmobil.com.br>
2017-03-11 13:23:46 -0300

    Criado atividade para detalhe

---
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -0,0 +1,22 @@
+package com.udacity.stockhawk.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.udacity.stockhawk.R;
+
+/**
+ * DetailActivity
+ * Created by Adalberto Fernandes Júnior on 11/03/17.
+ * Copyright © 2016. All rights reserved.
+ */
+
+public class DetailActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
